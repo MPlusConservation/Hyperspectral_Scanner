c6b4e33820aeec93f2b1ce4622c30a0c8872881c
M+ Conservation Team <102126222+MPlusConservation@users.noreply.github.com>
2023-05-01 11:24:14 +0800

    initial commit

--- src/hyperspectral_scanner/_widget.py
+++ src/hyperspectral_scanner/_widget.py
@@ -10,37 +10,62 @@ from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from napari.qt import thread_worker
+from PIL import Image, ImageOps
+import time
+import numpy as np
 
 if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
+tsr = Image.open("/Users/homelyp/Desktop/ghos.tif")
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+width, height = tsr.size #X and Y image dimensions
+#chose desired image width, for example, a width of 200 should be used if you have a slow machine
+X_dim = width #chose "width" if you want to use the whole image
+Y_dim = height #int(height*(X_dim/width)) #maintain aspect ration as original image
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+#get the data into a numpy array
+images = []
+for i in range(tsr.n_frames):
+    tsr.seek(i)
+    images.append(np.array(ImageOps.fit(tsr, (X_dim,Y_dim), Image.LANCZOS), dtype=np.float32)/ 255 ) 
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
-
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+scan = np.zeros((493,676,3))#initial blank image for display in Napari.
+#Spatial dimensionsis 676 (in this example). This should be a fixed value for the spectral camera. 
+#493 is the number of images collected. 3 is the 3-channel RGB dimension
 
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+@thread_worker
+
+def collect_scan(layer, interval=0.05):
+    update_scan = np.zeros((493,676,3)) #numpy array where color data is added for display in Napari 
+      
+    for i in range(492): #for loop takes one through the entire scan range
+         #images = camera.capture() #capture with Asi ZWO camera. Output is a numpy array.
+        
+        buffer=np.zeros((1,676,3)) #Buffer to hold sudo-color image for single scan line
+        buffer[:,:,0] = images[i][120] #red
+        buffer[:,:,1] = images[i][90] #green
+        buffer[:,:,2] = images[i][60] #blue
+         
+         
+        update_scan[i] = buffer #add sudo-color scan line to update array
+        layer.data = update_scan #update layer in Napari so one can visualize scan
+        yield
+        time.sleep(interval)
+    
+with napari.gui_qt():
+    v = napari.Viewer()#(show=False)
+      #v.window._qt_window.setWindowState(Qt.WindowMaximized)
+      # #v.show()
+      # image_layer = v.add_image(scan)
+      # worker = collect_scan(image_layer)
+      # #worker.start()
+    button = QPushButton("Start Scan")
+    button.clicked.connect(worker.start)
+    v.window.add_dock_widget(widget=button, area='right')
+
+
+
